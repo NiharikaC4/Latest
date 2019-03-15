7e04d94ba31d01984c32dccddf7e10f95cac52dd
NiharikaC4 <niharika.c4@gmail.com>
2019-03-15 14:39:50 +0530

    added

--- src/test/java/com/qaagility/controller/CalcTest.java
+++ src/test/java/com/qaagility/controller/CalcTest.java
@@ -2,6 +2,7 @@ package com.qaagility.controller;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import com.qaagility.javaee.*
  
 public class CalcTest {
 
@@ -20,6 +21,12 @@ public class CalcTest {
 	@Test	
       public void testCalc() {
         assertEquals("Result", 9, 9);
+
+	Calcmul obj1 = new Calcmul();
+	Calculator obj2 = new Calculator();
+
+	assertEquals(obj1.mul(),18);
+	assertEquals(obj2.add(),9);
       }
 
 	@Test	
